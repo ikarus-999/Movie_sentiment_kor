c6d421187b9e7639bc5e3dbc3f2079ceec6511ac
NyanCattEd <36034521+ikarus-999@users.noreply.github.com>
2019-10-14 17:34:29 +0900

    Update movie_flask.py

--- movie_flask.py
+++ movie_flask.py
@@ -1,13 +1,11 @@
-# import Flask
 from flask import Flask, request, make_response, jsonify
 import json
 from collections import OrderedDict
 from movie_search import search_response
-from naver_movie_api import getInfoFromNaver, get_movie_review
-from naver_movie_code_api import get_movie_code
-from pprint import pprint
+from naver_movie_api import getInfoFromNaver
 from movie_senti_anal import predict_pos_neg
 from konlpy.tag import Okt
+from gensim.models import word2vec
 
 # flask app 초기화
 app = Flask(__name__)
@@ -31,6 +29,7 @@ def results():
     a = ['movieNm','movieEn','directors','nationAlt','openDt','repGenreNm','typeNm']
     data = data['queryResult']['parameters']
 
+
     if data['movieNm'] != '':
         name_info = search_response.movie_name_dsl(data['movieNm'])
         file_data["fulfillmentText"] = """영화 : {0} 영어 : {1} 감독 : {2} 국가 : {3} 개봉일 : {4}
@@ -41,25 +40,42 @@ def results():
     elif data['openDt'] != '':
         release_info = search_response.movie_release_dsl(data['openDt'])
         file_data["fulfillmentText"] = '영화 : {0}'.format(release_info)
-     else:
-        sentence = data
-        x = predict_pos_neg(sentence)
-        score_info = search_response.scoresearch(x)
-        #file_data["fulfillmentText"] = score_info
 
-        sentences_tag = []
-        noun_list = []
-        okt = Okt()
-        morph = okt.pos(score_info)
-        sentences_tag.append(morph)
+    elif data['senti'] != '':
+        model = word2vec.Word2Vec.load("NaverMovie.model")
+        mal = model.wv.most_similar(positive=[data['senti']], topn=5)
+        print(mal[1])
+        # senti_info = search_response.scoresearch(mal[0] * 10)
+        # senti_list = ''.join(getInfoFromNaver(mal[0]))
 
-        for sentence1 in sentences_tag:
-            for word, tag in sentence1:
-                if tag in ['Noun']:
-                    noun_list.append(word)
-        noun_info = search_response.movie_name_dsl(noun_list[0])
-        file_data['fulfillmentText'] = ''.join(getInfoFromNaver(noun_info))
+        # file_data['fulfillmentText'] = ''.join(mal)
+        mal_max = max(mal)
+        
+        return {'fulfillmentText': ('%.1f' % mal_max[1])}
 
+        #file_data['fulfillmentText'] = model.most_similar(positive=[data_param['senti']])
+
+    # else:
+    #     sentence = data['queryResult']
+    #     x = predict_pos_neg(sentence)
+    #     score_info = search_response.scoresearch(x)
+    #     #file_data["fulfillmentText"] = score_info
+    #
+    #     sentences_tag = []
+    #     noun_list = []
+    #     okt = Okt()
+    #     morph = okt.pos(score_info)
+    #     sentences_tag.append(morph)
+    #
+    #     for sentence1 in sentences_tag:
+    #         for word, tag in sentence1:
+    #             if tag in ['Noun']:
+    #                 noun_list.append(word)
+    #     #noun_info = search_response.movie_name_dsl(noun_list[0])
+    #
+    #     movie_lists = ' '.join(getInfoFromNaver(noun_list))
+    #     file_data['fulfillmentText'] = movie_lists
+    #     # file_data['fulfillmentText'] = ''.join(getInfoFromNaver(noun_info))
 
 # app 실행
 if __name__ == '__main__':
